ffc75db8a1a2f138c1e20223821b870f22b119c7
Joshua Figueiredo <339727042@GoTVDSB.ca>
2024-09-24 09:32:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace topic_5_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber;
+            double numberGuess;
+            secretNumber = 7;
+            Console.Write("Guess a number: ");
+            Double.TryParse(Console.ReadLine(), out numberGuess);
+            if (numberGuess >= 11 || numberGuess <= 0)
+            {
+                Console.WriteLine("Out of range!");
+                Console.WriteLine("The secret number is " + secretNumber + " and the range is between 1-10");
+            }
+            else if (numberGuess > secretNumber || numberGuess < secretNumber)
+            {
+                Console.WriteLine("Incorrect!");
+                Console.WriteLine("The secret number was " + secretNumber + " and the range is between 1-10");
+            }
+            else
+                Console.WriteLine("GG! You got it correctly");
+                
+            
+        }
+    }
+}
